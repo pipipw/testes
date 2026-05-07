27e08d1b658612ff442ff844a34b493178ec8c71
pipipw <rafifairuz13@gmail.com>
2026-05-07 22:32:28 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE='galeri-v5';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+  // Always fetch fresh, no caching
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
